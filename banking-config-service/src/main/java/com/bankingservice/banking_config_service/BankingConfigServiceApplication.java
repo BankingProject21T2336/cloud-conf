3f845b04e86380fc3fa5f6955b6c4681b4499ca4
Ebai-Bate-Lucky-Betty <ebaibateluckybetty@gmail.com>
2025-09-27 15:20:22 +0100

    My first commit

---
+++ banking-config-service/src/main/java/com/bankingservice/banking_config_service/BankingConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bankingservice.banking_config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class BankingConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingConfigServiceApplication.class, args);
+	}
+
+}
---
+++ banking-config-service/src/test/java/com/bankingservice/banking_config_service/BankingConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingservice.banking_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
